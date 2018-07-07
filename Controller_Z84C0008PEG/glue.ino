24cbb1d3ca86f8746d45a428fd31c39f776506af
Magnus Runesson <magnus@pokewhat.com>
2018-07-07 11:51:13 +0200

    Added glue code that figure out what to do next based on chip status registers

--- Controller_Z84C0008PEG/glue.ino
+++ Controller_Z84C0008PEG/glue.ino
@@ -1,8 +1,45 @@
+const int GLUE_CHIPMASK_READ_INSTRUCTION    = CHIPMASK_ACTIVE_M1 | CHIPMASK_ACTIVE_RD | CHIPMASK_ACTIVE_MREQ;
+const int GLUE_CHIPMASK_READ_MEMORY         = CHIPMASK_ACTIVE_RD | CHIPMASK_ACTIVE_MREQ;
+const int GLUE_CHIPMASK_WRITE_MEMORY        = CHIPMASK_ACTIVE_WR | CHIPMASK_ACTIVE_MREQ;
+
+int glueHasReset;
+
+void glueSetup()
+{
+  glueHasReset = 0;
+}
+
 void glueFigureOutWhatsNext( unsigned char* _pszMessage )
 {
+  if( glueHasReset == 0 ) {
+    Serial.write( "Reset\n" );
+    chipReset();
+    glueHasReset = 1;
+    return;
+  }
+  
   int chipStatus = chipGetStatusMask();
   Serial.write( "Chip status mask: 0x" );
   serialWriteHex( chipStatus );
   Serial.write( "\n" );
+
+  if( chipStatus == GLUE_CHIPMASK_READ_INSTRUCTION )
+  {
+    Serial.write("Read instruction\n");
+    ioReadFromAddressWriteToData(first);
+    chipClockPulse();
+  } else if( chipStatus == GLUE_CHIPMASK_READ_MEMORY )
+  {
+    Serial.write("Read memory\n");
+    ioReadFromAddressWriteToData(first);
+    chipClockPulse();
+  } else if( chipStatus == GLUE_CHIPMASK_WRITE_MEMORY )
+  {
+    Serial.write("Write memory\n");
+  } else
+  {
+    Serial.write("Unknown\n");
+    chipClockPulse();
+  }
 }
 
