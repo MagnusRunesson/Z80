287d3ef4599da29c937d513aabb1a112bfddef6b
Magnus Runesson <magnus@pokewhat.com>
2018-07-10 20:34:31 +0200

    Wrote Z80 code that tries to write to every address pin to verify that each pin work.

--- first.asm
+++ first.asm
@@ -26,14 +26,52 @@ code_start  equ 0x0000
 #code   CODE, code_start
 
 ; define some code here
+;	jp $e0e0
 
-	nop
-	ld hl,janne
-	ld a,(hl)
-	ld hl,output
+	ld hl,$0200
 	ld (hl),a
+
+	;nop
+	;ld hl,janne
+	;ld a,(hl)
+
+	;ld hl,$4000
+	;ld (hl),a
+	;ld hl,$2000
+	;ld (hl),a
+	;ld hl,$1000
+	;ld (hl),a
+	;ld hl,$0800
+	;ld (hl),a
+	;ld hl,$0400
+	;ld (hl),a
+	ld hl,$0200
+	ld (hl),a
+	ld hl,$0100
+	ld (hl),a
+	ld hl,$0080
+	ld (hl),a
+	ld hl,$0040
+	ld (hl),a
+	ld hl,$0020
+	ld (hl),a
+	ld hl,$0010
+	ld (hl),a
+	ld hl,$0008
+	ld (hl),a
+	ld hl,$0004
+	ld (hl),a
+	ld hl,$0002
+	ld (hl),a
+	ld hl,$0001
+	ld (hl),a
+	;ld a,(hl)
+	;inc hl
+	;ld (hl),a
 
 	jr  $
 
 janne:	.db	$7f
 output: .db	$30
+
+ramtest: .db 0
