1d9d799aef62b48c9d12d6e9faeb5cdc1b2dcf2a
Magnus Runesson <magnus@pokewhat.com>
2018-08-30 11:48:51 +0200

    Moved a bunch of i2c code to a separate file, so it can be included / excluded when working on the SH1106 simulator

--- GPU/display_test_i2c/display_test_i2c.ino
+++ GPU/display_test_i2c/display_test_i2c.ino
@@ -1,10 +1,5 @@
-#include <util/twi.h>
 
-#define ADDR  0x78
-
-#define I2C_ERR_NONE 0x00
-#define I2C_ERR_TIMEOUT 0x01
-#define I2C_ERR_BUS 0x02
+#include "sh1106_shared.h"
 
 typedef unsigned char uint8;
 int pixel_x, pixel_y;
@@ -13,149 +8,13 @@ int testfestIndex = 0;
 
 int waitCycles;
 
+int frame;
+
 uint8 screen[ 128 * 64 / 8 ];
-
-void i2c_set_error(uint8_t code, uint8_t pos)
-{
-  Serial.write("I2C error code. Code: " );
-  serialWriteInt( code );
-  Serial.write(" - Pos: " );
-  serialWriteInt( pos );
-  Serial.write("\n");
-}
-
-void i2c_init()
-{
-  /*
-  TWBR: bit rate register
-  TWSR: status register (contains preselector bits)
-
-  prescalar
-    0    1
-    1   4
-    2   16
-    3   64
-
-  f = F_CPU/(16+2*TWBR*prescalar)
-  
-  F_CPU = 16MHz
-    TWBR = 152;
-    TWSR = 0;
-  --> 50KHz
-
-    TWBR = 72;
-    TWSR = 0;
-  --> 100KHz
-
-    TWBR = 12;
-    TWSR = 0;
-  --> 400KHz
-
-    F_CPU/(2*100000)-8  --> calculate TWBR value for 100KHz
-*/
-  TWSR = 0;
-  TWBR = F_CPU/(2*600000)-8;
-}
-
-uint8_t i2c_wait(uint8_t mask, uint8_t pos)
-{
-  uint32_t cnt = 6000; /* timout value should be > 280 for 50KHz Bus and 16 Mhz CPU, however the start condition might need longer */
-  while( !(TWCR & mask) )
-  {
-    if ( cnt == 0 )
-    {
-      i2c_set_error(I2C_ERR_TIMEOUT, pos);
-      return 0; /* error */
-    }
-    cnt--;
-    waitCycles++;
-  }
-  
-  return 1; /* all ok */
-}
-
-/* sla includes all 8 bits (with r/w bit), assums master transmit */
-uint8_t i2c_start(uint8_t sla)
-{
-  register uint8_t status;
-  
-  /* send start */
-  TWCR = _BV(TWINT) |  _BV(TWSTA)  |  _BV(TWEN);
-   
-  /* wait */
-  if ( i2c_wait(_BV(TWINT), 1) == 0 )
-    return 0;
-  
-  status = TW_STATUS;
- 
-  /* check status after start */  
-  if ( status != TW_START && status != TW_REP_START )
-  {
-    i2c_set_error(I2C_ERR_BUS, 1);
-    return 0;
-  }
-
-  /* set slave address */  
-  TWDR = sla;
-  
-  /* enable sla transfer */
-  TWCR = _BV(TWINT)  |  _BV(TWEN);
-
-  /* wait */
-  if ( i2c_wait(_BV(TWINT), 2) == 0 )
-    return 0;
-
-  status = TW_STATUS;
-  /* check status after sla */  
-  if ( status != TW_MT_SLA_ACK )
-  {
-    i2c_set_error(I2C_ERR_BUS, 2);
-    return 0;
-  }
-
-   return 1;
-}
-
-void i2c_stop(void)
-{
-  /* write stop */
-  TWCR = _BV(TWINT) | _BV(TWEN) | _BV(TWSTO);
-
-  /* no error is checked for the stop condition */  
-  i2c_wait(_BV(TWSTO), 4);
-}
-
-uint8_t i2c_send(uint8_t data)
-{
-  register uint8_t status;
-  TWDR = data;
-  TWCR = _BV(TWINT)  |  _BV(TWEN);
-  if ( i2c_wait(_BV(TWINT), 3) == 0 )
-    return 0;
-
-  status = TW_STATUS;
-  if ( status != TW_MT_DATA_ACK )
-  {
-    i2c_set_error(I2C_ERR_BUS, 3);
-    return 0;
-  }
-
-  return 1;  
-}
-
-/*
-uint8_t i2c_read(bool isLastByte) {
-
-  if (isLastByte) TWCR = (1 << TWINT) | (1 << TWEN);
-  else TWCR = (1 << TWEA) | (1 << TWINT) | (1 << TWEN);
-  while (!(TWCR & (1 << TWINT)));
-  return TWDR;
-}
-*/
 
 void display_init() {
   i2c_start(ADDR);
-  i2c_send(0x00); //................ control byte
+  i2c_send(CONTROL_SENDCOMMAND); //................ control byte
   i2c_send(0xAE); //................ displayOff
   i2c_send(0x8D); //................   *
   i2c_send(0xD5); i2c_send(0x80); // clockFrequency
@@ -176,7 +35,7 @@ void display_init() {
 void i2c_sendcommand(int _cmd)
 {
   i2c_start(ADDR);
-  i2c_send(0x80);
+  i2c_send(CONTROL_SENDCOMMAND);
   i2c_send(_cmd);
   i2c_stop();
 }
@@ -188,12 +47,12 @@ void blit()
   for( page=0; page<8; page++ )
   {
     i2c_start(ADDR);
-    i2c_send(0x00); //................ control byte
+    i2c_send(CONTROL_SENDCOMMAND); //................ control byte
     i2c_send(0xB0 + page); //................ pageAddr
     i2c_stop();
 
     i2c_start(ADDR);
-    i2c_send(0x40);
+    i2c_send(CONTROL_SENDDATA);
 
     // Left side off screen
     i2c_send( 0 );
@@ -237,19 +96,31 @@ void blit()
   }
 }
 
-void blit_image( unsigned char* _pImage )
+void blit_image( const unsigned char* _pImage )
 {
   int page;
+
+/*
+  i2c_sendcommand(0x40 + frame); //................ startLine
+  frame++;
+  if( frame >= 64 )
+  {
+    frame -= 64;
+  }
+  delay(20);
+*/
+
+
 
   for( page=0; page<8; page++ )
   {
     i2c_start(ADDR);
-    i2c_send(0x00); //................ control byte
+    i2c_send(CONTROL_SENDCOMMAND); //................ control byte
     i2c_send(0xB0 + page); //................ pageAddr
     i2c_stop();
 
     i2c_start(ADDR);
-    i2c_send(0x40);
+    i2c_send(CONTROL_SENDDATA);
 
     // Left side off screen
     i2c_send( 0 );
@@ -283,12 +154,12 @@ void testfest()
   for( j=0; j<8; j++ )
   {
     i2c_start(ADDR);
-    i2c_send(0x00); //................ control byte
+    i2c_send(CONTROL_SENDCOMMAND); //................ control byte
     i2c_send(0xB0 + j); //................ pageAddr
     i2c_stop();
 
     i2c_start(ADDR);
-    i2c_send(0x40);
+    i2c_send(CONTROL_SENDDATA);
 
     int i;
     for( i=0; i<132; i++ ) {
@@ -383,6 +254,8 @@ void setup() {
   pixel_y = 7;
   move_x = 1;
   move_y = 1;
+
+  frame = 0;
 }
 
 #include "test0.h"
@@ -390,13 +263,14 @@ void setup() {
 #include "test2.h"
 #include "test3.h"
 #include "test4.h"
+#include "tilemap_dungeon_8x8.h"
 
 void loop() 
 {
   waitCycles = 0;
   //clearScreen();
   //updatePixel();
-  blit_image( test4_pixels );
+  blit_image( tilemap_dungeon_8x8_pixels );
   //testfest();
 
   /*
